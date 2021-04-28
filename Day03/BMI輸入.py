91aec7c83dbffb2f7881cfef1607df7e8514f1a1
Iris <ychueh@gmail.com>
2021-04-28 19:32:19 +0800

    aaa

---
+++ Day03/BMI輸入.py
@@ -0,0 +1,11 @@
+h = input('請輸入身高')
+w = input('請輸入體重')
+print(h, type(h))
+print(w, type(w))
+h = float(h)  # 將str轉成floot
+w = float(w)  # 將str轉成floot
+print(h, type(h))
+print(w, type(w))
+bmi = w / ((h/100) ** 2)
+print("%.2f" % bmi)
+
